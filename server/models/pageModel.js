4c75a61c6663fdb3d486fd6a2ccc19a86538b758
Oshri Kdoshim <cms.x.seo@gmail.com>
2018-10-19 10:31:07 +0300

    Update schema

--- server/models/pageModel.js
+++ server/models/pageModel.js
@@ -2,9 +2,27 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const pageModel = new schema({
-    _id: { type: mongoose.Schema.Types.ObjectId } ,
-    name: { type: String},
-    description: { type: String}
+    _id: { 
+        type: mongoose.Schema.Types.ObjectId
+    },
+    name: { 
+        type: String,
+        unique: true,
+        required: true
+    },
+    link: { 
+        type: String
+    },
+    description: { 
+        type: String
+    },
+    active: {
+        type: Boolean,
+        default: true
+    },
+    parent: {
+        type: String
+    }
 });
 
 module.exports = mongoose.model('Page', pageModel);
