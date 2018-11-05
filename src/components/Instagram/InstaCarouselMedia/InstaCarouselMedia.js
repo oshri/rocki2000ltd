8fb685391d694cf7da7f209dfe728c41d437b691
Oshri Kdoshim <cms.x.seo@gmail.com>
2018-11-06 01:47:50 +0200

    Support instegram carousel media

---
+++ src/components/Instagram/InstaCarouselMedia/InstaCarouselMedia.js
@@ -0,0 +1,113 @@
+import react, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+	Carousel,
+	CarouselItem,
+	CarouselControl,
+	CarouselIndicators,
+	CarouselCaption
+} from 'reactstrap';
+import './InstaCarouselMedia.scss';
+
+class InstaCarouselMedia extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { activeIndex: 0 };
+		this.next = this.next.bind(this);
+		this.previous = this.previous.bind(this);
+		this.goToIndex = this.goToIndex.bind(this);
+		this.onExiting = this.onExiting.bind(this);
+        this.onExited = this.onExited.bind(this);
+        this.slides = [];
+    }
+
+
+	onExiting() {
+		this.animating = true;
+	}
+
+	onExited() {
+		this.animating = false;
+	}
+
+	next() {
+		if (this.animating) return;
+		const nextIndex =
+			this.state.activeIndex === this.carouselMedia.length - 1
+				? 0
+				: this.state.activeIndex + 1;
+		this.setState({ activeIndex: nextIndex });
+	}
+
+	previous() {
+		if (this.animating) return;
+		const nextIndex =
+			this.state.activeIndex === 0
+				? this.carouselMedia.length - 1
+				: this.state.activeIndex - 1;
+		this.setState({ activeIndex: nextIndex });
+	}
+
+	goToIndex(newIndex) {
+		if (this.animating) return;
+		this.setState({ activeIndex: newIndex });
+	}
+
+	render() {
+        const { activeIndex } = this.state;
+        
+        this.carouselMedia = this.props.carouselMedia.map((media, index) => {
+            return {
+                id: `slide-${index}`,
+                src: media.images.standard_resolution.url
+            }
+        });
+
+
+		const slides = this.carouselMedia.map((media, index) => {
+			return (
+				<CarouselItem
+					className="custom-tag"
+					tag="div"
+					key={media.id}
+					onExiting={this.onExiting}
+					onExited={this.onExited}
+				>
+                 <img src={media.src} />
+				</CarouselItem>
+			);
+		});
+
+		return (
+			<Carousel
+                className="InstaCarouselMedia"
+                activeIndex={activeIndex}
+                next={this.next}
+                previous={this.previous}>
+
+                <CarouselIndicators
+                    items={this.carouselMedia}
+                    activeIndex={activeIndex}
+                    onClickHandler={this.goToIndex}
+                />
+                {slides}
+                <CarouselControl
+                    direction="prev"
+                    directionText="Previous"
+                    onClickHandler={this.previous}
+                />
+                <CarouselControl
+                    direction="next"
+                    directionText="Next"
+                    onClickHandler={this.next}
+                />
+            </Carousel>
+		);
+	}
+}
+
+InstaCarouselMedia.propTypes = {
+	carouselMedia: PropTypes.arrayOf(PropTypes.object)
+};
+
+export default InstaCarouselMedia;
--- src/components/Instagram/InstaPost/InstaPost.js
+++ src/components/Instagram/InstaPost/InstaPost.js
@@ -1,63 +1,96 @@
+import react, { Component } from 'react';
 import PropTypes from 'prop-types';
 import TextTruncate from '../../TextTruncate';
-import { Player, ControlBar, PlayToggle} from 'video-react';
+import { Player, ControlBar, PlayToggle } from 'video-react';
 import './InstaPost.scss';
+import InstaCarouselMedia from '../InstaCarouselMedia';
 
-const InstaPost = (props) => { 
-    const { tag }  = props;
+
+const InstaPost = (props) => {
+    const {tag} = props;
+	
+	const renderMediaByType = () => {
+		if (tag.carousel_media) {
+            return (<InstaCarouselMedia carouselMedia={tag.carousel_media}/>)
+		} else if (tag.videos) {
+		} else {
+		}
+	};
 
     return (
         <div className="InstaPost">
             <div className="InstaHeader">
                 <div className="InstaAvatarContainer">
                     <a className="Avatar" target="_blank">
-                        <img src={tag.user.profile_picture}/>
+                        <img src={tag.user.profile_picture} />
                     </a>
                 </div>
                 <div className="InstaHeaderText">
-                    <a className="Username" href={tag.link} target="_blank">
-                        <span className="UsernameText">{tag.user.username.toUpperCase()}</span>
+                    <a
+                        className="Username"
+                        href={tag.link}
+                        target="_blank"
+                    >
+                        <span className="UsernameText">
+                            {tag.user.username.toUpperCase()}
+                        </span>
                     </a>
                     <span className="FollowerCountText">
                         {tag.user.full_name}
                     </span>
                 </div>
-                <a className="LInkProfileButton" href={tag.link} target="_blank">
-                    <span className="InstaEmbedSpriteComment embedSpriteGlyph"></span>
+                <a
+                    className="LInkProfileButton"
+                    href={tag.link}
+                    target="_blank"
+                >
+                    <span className="InstaEmbedSpriteComment embedSpriteGlyph" />
                 </a>
             </div>
             <div className="InstaContent">
-                <a className="EmbeddedMedia" href={tag.link} target="_blank">
-                    <img className="EmbeddedMediaImage" src={tag.images.standard_resolution.url}/>
-                </a>
+                {/* <a
+                    className="EmbeddedMedia"
+                    href={tag.link}
+                    target="_blank"
+                >
+                    <img
+                        className="EmbeddedMediaImage"
+                        src={tag.images.standard_resolution.url}
+                    />
+                </a> */}
+                <div className="EmbeddedMedia">
+                    {renderMediaByType()}
+                </div>
             </div>
             <div className="InstaFeedback">
                 <a className="Likes" href={tag.link} target="_blank">
-                    <span className="InstaEmbedSpriteComment embedSpriteHeartOpen"></span>
-                    <span className="LiksCountText">{tag.likes.count}</span>
+                    <span className="InstaEmbedSpriteComment embedSpriteHeartOpen" />
+                    <span className="LiksCountText">
+                        {tag.likes.count}
+                    </span>
                 </a>
                 {/* <a className="Comments" href="" target="_blank">
                     <span className="InstaEmbedSpriteComment embedSpriteComment"></span>
                 </a> */}
                 <a className="Save" href={tag.link} target="_blank">
-                    <span className="InstaEmbedSpriteComment embedSpriteSaveOpen"></span>
+                    <span className="InstaEmbedSpriteComment embedSpriteSaveOpen" />
                 </a>
             </div>
             <div className="InstaDescription">
                 <TextTruncate
-                            className="Description"
-                            line={3}
-                            truncateText="…"
-                            text={tag.caption.text}
-                        />
+                    className="Description"
+                    line={3}
+                    truncateText="…"
+                    text={tag.caption.text}
+                />
             </div>
         </div>
-    )
-};
+    );
+	
+}
 
 InstaPost.propTypes = {
-    tag: PropTypes.object
+	tag: PropTypes.object
 };
 
 export default InstaPost;
-
