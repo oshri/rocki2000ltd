f8d1ea83098925e4ad033213da3aef9a0de7b914
Oshri Kdoshim <cms.x.seo@gmail.com>
2018-11-07 09:29:47 +0200

    Add Cache

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const { parse } = require('url');
 const bodyParser = require('body-parser');
 const Page = require('./models/pageModel');
 const helmet = require('helmet');
+const LRUCache = require('lru-cache');
 
 /**
  * CONFIG
@@ -22,15 +23,36 @@ dotenv.load({ path: '.env' });
 /**
  * APPLICATION LOGGING
  */
-// winston.configure({
-// 	transports: [
-// 		new winston.transports.Console({
-// 			timestamp: true,
-// 			label: 'Rocki2000ltd',
-// 			level: 'info'
-// 		})
-// 	]
-// });
+
+winston.configure({
+	transports: [
+		new winston.transports.Console({
+			timestamp: true,
+			label: 'Rocki2000ltd',
+			level: 'info'
+		})
+	]
+});
+
+/**
+ * SSR Cache
+ * production: 1 hour chach
+ */
+
+let cacheTime = 1000 * 60 * 60;
+
+if (process.env.NODE_ENV !== 'production') {
+	cacheTime = 100;
+}
+
+const ssrCache = new LRUCache({
+	max: 100,
+	maxAge: cacheTime
+});
+
+const getCacheKey = req => {
+	return `${req.url}`;
+};
 
 /**
  * MONGODB CONNECTION
@@ -51,6 +73,33 @@ app.prepare()
 		expressApp.use(helmet());
 
 		/**
+		 * SSR Chach Html
+		 */
+		const renderAndCache = (req, res, pagePath, queryParams) => {
+			const key = getCacheKey(req);
+
+			// If we have a page in the cache, let's serve it
+			if (ssrCache.has(key)) {
+				winston.log('CACHE HIT', `${key}`);
+				res.send(ssrCache.get(key));
+				return;
+			}
+
+			// If not let's render the page into HTML
+			app.renderToHTML(req, res, pagePath, queryParams)
+				.then(html => {
+					// Let's cache this page
+					winston.log('CACHE MISS', `${key}`);
+					ssrCache.set(key, html);
+
+					res.send(html);
+				})
+				.catch(err => {
+					app.renderError(err, req, res, pagePath, queryParams);
+				});
+		};
+
+		/**
 		 * Api Routes
 		 */
 
@@ -61,6 +110,10 @@ app.prepare()
 		 * Public Routes
 		 */
 
+		expressApp.get('/', (req, res) => {
+			renderAndCache(req, res, '/');
+		});
+
 		expressApp.get('/page/:link', (req, res) => {
 			const queryParams = {
 				link: req.params.link
@@ -69,17 +122,17 @@ app.prepare()
 			Page.findOne({ link: queryParams.link })
 				.exec()
 				.then(page => {
-					if(!page) res.redirect(301, '/');
+					if (!page) res.redirect(301, '/');
 
-					return app.render(req, res, '/page', { id: page.id });
+					return renderAndCache(req, res, '/page', { id: page.id });
 				})
-				.catch((err) => res.redirect(301, '/'));
+				.catch(err => res.redirect(301, '/'));
 		});
 
 		expressApp.get('/page', (req, res) => {
 			const queryParams = { id: req.params.id, link: req.params.link };
 			if (req.params.id)
-				return res.redirect(`/page/${req.params.link}`, queryParams);
+				return renderAndCache(`/page/${req.params.link}`, queryParams);
 			res.redirect(301, '/');
 		});
 
@@ -91,17 +144,17 @@ app.prepare()
 			Page.findOne({ link: queryParams.link })
 				.exec()
 				.then(page => {
-					if(!page) res.redirect(301, '/');
+					if (!page) res.redirect(301, '/');
 
-					return app.render(req, res, '/subject', { id: page.id });
+					return renderAndCache(req, res, '/subject', { id: page.id });
 				})
-				.catch((err) => res.redirect(301, '/'));
+				.catch(err => res.redirect(301, '/'));
 		});
 
 		expressApp.get('/subject', (req, res) => {
 			const queryParams = { id: req.params.id, link: req.params.link };
 			if (req.params.id)
-				return res.redirect(`/subject/${req.params.link}`, queryParams);
+				return renderAndCache(`/subject/${req.params.link}`, queryParams);
 			res.redirect(301, '/');
 		});
 
@@ -111,10 +164,10 @@ app.prepare()
 
 		expressApp.get('*', (req, res) => {
 			if (req.url === '/sw.js') {
-				app.serveStatic(req, res, path.resolve('./static/sw.js'))
-			  } else {
+				app.serveStatic(req, res, path.resolve('./static/sw.js'));
+			} else {
 				return handle(req, res);
-			  }
+			}
 		});
 
 		expressApp.listen(PORT, () =>
